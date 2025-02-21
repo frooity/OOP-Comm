d420a946367b4a269560ec3c0303abae9cfc34e8
frooity <frooitism@gmail.com>
2025-02-21 23:50:27 +0700

    Remove asg1 & asg2

--- asg2/src/Author.java
+++
@@ -1,9 +0,0 @@
-public class Author{
-    public String name;
-    public String skills;
-    
-    public Author(String name, String skills){
-        this.name = name;
-        this.skills = skills;
-    }
-}
